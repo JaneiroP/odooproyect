1f79f45dc3cd708540f08fa8a1eb2c61b35ab8bb
JaneiroP <janeiroplacido1944@gamil.com>
2023-04-05 22:21:28 -0400

    uploading repository

--- peliculas/models/presupuesto_model.py
+++ peliculas/models/presupuesto_model.py
@@ -44,6 +44,7 @@ class PresupuestoModel(models.Model):
         comodel_name = 'genero',string='Genero')
     description = fields.Text('Descripcion')
     link_trailer = fields.Char()
+
     is_book = fields.Boolean('Version libro')
     book = fields.Binary('Libro')
     book_filename = fields.Char()
