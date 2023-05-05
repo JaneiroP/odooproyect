8eb99479211ca56a303eaa7aff0e444c901f5cc7
JaneiroP <janeiroplacido1944@gamil.com>
2023-05-05 05:49:09 -0400

    uploading repository

--- peliculas/wizard/create_uom_wizard.py
+++ peliculas/wizard/create_uom_wizard.py
@@ -4,15 +4,15 @@ from odoo import fields, models, api
 class CreateUomWizard(models.TransientModel):
     _name = 'create.uom.wizard'
 
-    uom_id = fields.Many2one('uom.uom', string='Base UOM')
-    name = fields.Char(string='Name')
-    factor = fields.Float(string='Conversion Factor')
+    unit_value = fields.Float(string='Unit Value')
+    uom_id = fields.Many2one('uom.uom', string='Referenced UoM')
+
 
     def create_uom(self):
         uom_obj = self.env['uom.uom']
         uom_data = {
-            'name': self.name,
-            'factor': self.factor,
+            'name': f'{self.unit_value} {self.uom_id}',
+            'factor': self.factor, #falta factor value
             'category_id': self.uom_id.category_id.id,
             'uom_type': self.uom_id.uom_type,
             'rounding': self.uom_id.rounding,
